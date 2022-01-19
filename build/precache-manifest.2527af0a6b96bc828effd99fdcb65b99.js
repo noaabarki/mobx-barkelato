d728c7032020aff8a1e1e073ecf0d0aa7301b7fb
Noaa Barki <noaabarki@Noaas-MBP.lan>
2022-01-19 23:05:40 +0200

    feat: dockerize the app and added Kubernetes deployment

---
+++ build/precache-manifest.2527af0a6b96bc828effd99fdcb65b99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6ef2d29af7da51509ad1adff5df691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e689e2a60520c9c87539",
+    "url": "/static/js/2.87e12079.chunk.js"
+  },
+  {
+    "revision": "ef59c53ac9aa82f7bdf16920409b34b8",
+    "url": "/static/js/2.87e12079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ea4c18f252ca8e2bfb",
+    "url": "/static/js/main.333e4c0a.chunk.js"
+  },
+  {
+    "revision": "01cdc152e005d7c538a4",
+    "url": "/static/js/runtime-main.3ffb7372.js"
+  }
+]);
